64907de46cfc30dfd4d8b76af3c78747c5a343d4
Frédérique Mittelstaedt <git@gfm.io>
2025-11-17 03:16:26 +0000

    add Simplify type to simplify computed merge types

--- src/utils/merge.ts
+++ src/utils/merge.ts
@@ -1,7 +1,9 @@
 import type { Options } from "tsup";
 
+export type Simplify<T> = T extends infer U ? { [K in keyof U]: U[K] } : never;
 export type Merge2<T extends Partial<Options>, U extends Partial<Options>> = Omit<T, keyof U> & U;
-export type Merge<T extends Array<Partial<Options>>> = T extends [] ? Record<never, never> : T extends [infer U] ? U : T extends [infer U, ...infer R] ?  U extends Partial<Options> ? R extends Array<Partial<Options>> ? Merge2<U, Merge<R>> : never : never : never;
+export type RawMerge<T extends Array<Partial<Options>>> = T extends [] ? Record<never, never> : T extends [infer U] ? U : T extends [infer U, ...infer R] ?  U extends Partial<Options> ? R extends Array<Partial<Options>> ? Merge2<U, Merge<R>> : never : never : never;
+export type Merge<T extends Array<Partial<Options>>> = Simplify<RawMerge<T>>;
 
 /** Utility function for merging tsup configs and/or partial configs */
 export function merge(): Record<never, never>;
