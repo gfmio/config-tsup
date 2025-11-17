f0e6b790b4faa066f08f695328eece7ff1ccc31b
Frédérique Mittelstaedt <git@gfm.io>
2025-11-17 03:54:08 +0000

    feat: enforce valid Options keys in partial tsup config utility

--- src/utils/partial.ts
+++ src/utils/partial.ts
@@ -1,4 +1,11 @@
 import type { Options } from "tsup";
 
-/** Utility function for defining a partial tsup config */
-export const partial = <const T extends Partial<Options>>(options: T) => options;
+/**
+ * Utility function for defining a partial tsup config
+ * This version strictly enforces that only valid Options keys are allowed
+ */
+export const partial = <const T extends Partial<Options>>(
+  options: T extends Partial<Options> ?
+    keyof T extends keyof Options ? T : never
+    : never
+) => options;
